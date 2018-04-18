692e8430bd9cc403dd95dd3c0249a1e5d26b603e
Ross_Metzger <metzgerrf@gmail.com>
2018-04-17 20:56:46 -0400

    Initial commit

---
+++ PoolLevelMonitor/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace PoolLevelMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Measure = new System.Windows.Forms.Button();
+            this.rtb_RawData = new System.Windows.Forms.RichTextBox();
+            this.rtb_Readings = new System.Windows.Forms.RichTextBox();
+            this.tb_Base = new System.Windows.Forms.TextBox();
+            this.btn_ResetBase = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_LevelChange = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_ErrorList = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Measure
+            // 
+            this.btn_Measure.Location = new System.Drawing.Point(739, 12);
+            this.btn_Measure.Name = "btn_Measure";
+            this.btn_Measure.Size = new System.Drawing.Size(124, 23);
+            this.btn_Measure.TabIndex = 0;
+            this.btn_Measure.Text = "Measure";
+            this.btn_Measure.UseVisualStyleBackColor = true;
+            this.btn_Measure.Click += new System.EventHandler(this.btn_Measure_Click);
+            // 
+            // rtb_RawData
+            // 
+            this.rtb_RawData.Location = new System.Drawing.Point(71, 86);
+            this.rtb_RawData.Name = "rtb_RawData";
+            this.rtb_RawData.Size = new System.Drawing.Size(142, 447);
+            this.rtb_RawData.TabIndex = 1;
+            this.rtb_RawData.Text = "";
+            // 
+            // rtb_Readings
+            // 
+            this.rtb_Readings.Location = new System.Drawing.Point(340, 86);
+            this.rtb_Readings.Name = "rtb_Readings";
+            this.rtb_Readings.Size = new System.Drawing.Size(366, 447);
+            this.rtb_Readings.TabIndex = 2;
+            this.rtb_Readings.Text = "";
+            // 
+            // tb_Base
+            // 
+            this.tb_Base.Location = new System.Drawing.Point(289, 11);
+            this.tb_Base.Name = "tb_Base";
+            this.tb_Base.Size = new System.Drawing.Size(100, 20);
+            this.tb_Base.TabIndex = 3;
+            this.tb_Base.Text = "0.0";
+            this.tb_Base.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn_ResetBase
+            // 
+            this.btn_ResetBase.Location = new System.Drawing.Point(510, 11);
+            this.btn_ResetBase.Name = "btn_ResetBase";
+            this.btn_ResetBase.Size = new System.Drawing.Size(75, 23);
+            this.btn_ResetBase.TabIndex = 4;
+            this.btn_ResetBase.Text = "Reset Base";
+            this.btn_ResetBase.UseVisualStyleBackColor = true;
+            this.btn_ResetBase.Click += new System.EventHandler(this.btn_ResetBase_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(396, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "cm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(396, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "in";
+            // 
+            // tb_LevelChange
+            // 
+            this.tb_LevelChange.Location = new System.Drawing.Point(289, 37);
+            this.tb_LevelChange.Name = "tb_LevelChange";
+            this.tb_LevelChange.Size = new System.Drawing.Size(100, 20);
+            this.tb_LevelChange.TabIndex = 6;
+            this.tb_LevelChange.Text = "0.0";
+            this.tb_LevelChange.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(223, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Base Level";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(226, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Change";
+            // 
+            // tb_ErrorList
+            // 
+            this.tb_ErrorList.Location = new System.Drawing.Point(105, 590);
+            this.tb_ErrorList.Multiline = true;
+            this.tb_ErrorList.Name = "tb_ErrorList";
+            this.tb_ErrorList.Size = new System.Drawing.Size(588, 100);
+            this.tb_ErrorList.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 728);
+            this.Controls.Add(this.tb_ErrorList);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_LevelChange);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_ResetBase);
+            this.Controls.Add(this.tb_Base);
+            this.Controls.Add(this.rtb_Readings);
+            this.Controls.Add(this.rtb_RawData);
+            this.Controls.Add(this.btn_Measure);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Measure;
+        private System.Windows.Forms.RichTextBox rtb_RawData;
+        private System.Windows.Forms.RichTextBox rtb_Readings;
+        private System.Windows.Forms.TextBox tb_Base;
+        private System.Windows.Forms.Button btn_ResetBase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_LevelChange;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_ErrorList;
+    }
+}
+
---
+++ PoolLevelMonitor/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Threading;
+using System.IO.Ports;
+using System.IO;
+
+namespace PoolLevelMonitor
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextCallback(string text);
+
+        delegate void SetErrorCallback(string text);
+
+        delegate void MeasureLevelCallback( );
+
+        private static System.Timers.Timer aTimer;
+        const string FILE_PATH = @"/home/pi/DistanceSensor/";
+        const string FILE_NAME = @"PoolLog.txt";
+        const int TRIAL_SIZE = 20;
+        int readingCount = 0;
+        public int timerLength = 20;
+        public int rtbLineCount = 0;
+        public int rtbRawCount = 0;
+        public double sum = 0, ave = 0, baseLevel = 0.0, baseChange = -10.0;
+       // decimal[] rawData = new decimal[TRIAL_SIZE];
+        double[] rawData = new double[TRIAL_SIZE];
+    
+        public Form1()
+        {
+            InitializeComponent();
+            SetTimer();
+        }
+        public void MeasureLevel()
+        {
+            
+            //sets the number of data points taken per entry
+           // rtb_RawData.Clear();
+            
+            try
+            {
+                SetRawText("");
+                string stringToWrite;
+                int tries = 20;
+               // SerialPort ardP = new SerialPort("/dev/ttyACM0", 9600, Parity.None, 8);   //serial port for pi             
+                SerialPort ardP = new SerialPort("COM4", 9600, Parity.None, 8);           //serial port on pc
+
+                //decimal[] rawData = new decimal[TRIAL_SIZE];
+                
+                ardP.Open(); //open serial port
+                Thread.Sleep(500); //delay 500 millis
+                //if (2 == 2) return;
+                for (int i = 0; i < tries; i++)
+                {                    
+                    ardP.WriteLine("");                 //sends command to arduino to take reading
+                    Thread.Sleep(200);                  //delay to allow reading to complete
+                    rawData[i] = ReadSerial(ardP);      //calls read function and returns distance
+                    
+                    
+                   // rtb_RawData.AppendText(i.ToString() + "  " + rawData[i].ToString("F3") + "  " + (rawData[i]/2.54).ToString("F3") + "\n");
+                    SetRawText(i.ToString() + "  " + rawData[i].ToString("F3") + "  " + (rawData[i] / 2.54).ToString("F3") + "\n");
+                    if (rawData[i] == 0)                //if pi reads a distance of 0, data is thrown out
+                        i--;
+                }
+                ardP.Close();                           //serial port closed
+               // if (2 == 2) return;
+                sum = 0.0;
+                for (int i = 0; i < tries; i++)
+                {
+                    sum = sum + rawData[i];
+                }
+                ave = sum/tries;
+                if (baseLevel == -10.0)
+                {
+                    tb_Base.Text = ave.ToString();
+                }
+                baseLevel = Convert.ToDouble(tb_Base.Text);
+
+                baseChange = (ave - baseLevel)/2.54;
+               // tb_LevelChange.Text = baseChange.ToString("F3");
+
+                // WriteToFile(ave.ToString("F2"));      //average of of data points rounded to mm written to file                       
+                stringToWrite = "  " + ave.ToString("F3") + "cm  " + (ave / 2.54).ToString("F3") + "in  " + baseChange.ToString("F3") + " chg  " + DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+                WriteToFile(stringToWrite);
+                readingCount++;
+                if (readingCount > 20)
+                {
+                    readingCount = 0;
+                   // rtb_Readings.Clear();
+                    SetRawText("");
+                }
+                SetText(stringToWrite + "\n");
+               // rtb_Readings.AppendText(stringToWrite + "\n");
+                // ardP.Close();                           //serial port closed
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("From Measure Level " + e.Message);
+               // Console.ReadLine();                
+            }
+
+        }
+        //*******************************************************************************************
+
+        private void SetRawText(string text)
+        {
+            // InvokeRequired compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            try
+            {
+
+                if (this.rtb_RawData.InvokeRequired)
+                {
+                    SetTextCallback d = new SetTextCallback(SetRawText);
+                    this.Invoke(d, new object[] { text });
+                }
+                else
+                {
+                    if (rtbRawCount > 500 || text == "")
+                    {
+                        rtb_RawData.ResetText();
+                        rtbRawCount = 0;
+                        string dateString = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+                        tb_ErrorList.AppendText(dateString + "  Reset rtb_RawData \n");
+                    }
+                    this.rtb_RawData.AppendText(text);
+                    rtbRawCount++;
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                SetErrorText("SetRawText error " + ex.Message + "\n");
+            }
+
+        }
+        private void SetText(string text)
+        {
+            // InvokeRequired compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            try
+            {
+
+                if (this.rtb_Readings.InvokeRequired)
+                {
+                    SetTextCallback d = new SetTextCallback(SetText);
+                    this.Invoke(d, new object[] { text });
+                }
+                else
+                {
+                    if (rtbLineCount > 500 || text == "" )
+                    {
+                        rtb_Readings.ResetText();
+                        rtbLineCount = 0;
+                        string dateString = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+                        tb_ErrorList.AppendText(dateString + "  Reset rtb_Readings \n");
+                    }
+                    this.rtb_Readings.AppendText(text);
+                    rtbLineCount++;
+                    tb_LevelChange.Text = baseChange.ToString("F3");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                SetErrorText("SetText error " + ex.Message + "\n");
+            }
+
+        }
+        //*******************************************************************************************
+        private void SetErrorText(string text)
+        {
+            // InvokeRequired compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            try
+            {
+
+                if (this.tb_ErrorList.InvokeRequired)
+                {
+                    SetErrorCallback d = new SetErrorCallback(SetErrorText);
+                    this.Invoke(d, new object[] { text });
+                }
+                else
+                {
+                    this.tb_ErrorList.AppendText(text);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                // If we get an error here put it in the datalist
+                SetText("SetText error " + ex.Message + "\n");
+            }
+
+        }
+        private void SetTimer()
+        {
+            // Create a timer 
+            // timerLength is in seconds. Timer expects milliseconds
+            aTimer = new System.Timers.Timer(timerLength * 1000);
+            // Hook up the Elapsed event for the timer. 
+
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+
+        }
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            timerLength = 30;
+            aTimer.Interval = timerLength * 1000;
+            MeasureLevel();
+        }
+        private static double ReadSerial(SerialPort sp)
+        {
+            string serialDat = "";
+            try
+            {
+                
+                byte[] buf = new byte[sp.BytesToRead];
+
+                if (sp.BytesToRead == 0)        //if 0 bytes to read, serial data is not ready yet
+                {
+                    Thread.Sleep(100);
+                    return 0;
+                }
+                sp.Read(buf, 0, buf.Length);    //reads serial from arduino           
+
+                foreach (Byte b in buf)         //constructs the distance string one byte at a time
+                {
+                    if (Convert.ToChar(b) != '\n')       //checks for newline character and removes it from end of string
+                        serialDat += Convert.ToChar(b);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("From ReadSerial " + e.Message);
+                //Console.ReadLine();
+                serialDat = "";
+            }
+            return Convert.ToDouble(serialDat);    //converts distance string to decimal and returns value
+        }
+
+        private static void WriteToFile(string distance)
+        {
+            string fullPath = FILE_PATH + FILE_NAME;
+            string stringToWrite;
+           // stringToWrite = distance + " cm " + DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+            stringToWrite = distance;
+            
+            using (StreamWriter sw = new StreamWriter(fullPath, append: true))   //create streamwrite
+            {
+                
+                sw.WriteLine(stringToWrite);             //write to text file
+                
+            }
+            return;
+        }
+
+        private void btn_Measure_Click(object sender, System.EventArgs e)
+        {
+            MeasureLevel();
+        }
+
+        private void btn_ResetBase_Click(object sender, EventArgs e)
+        {
+            tb_Base.Text = ave.ToString();
+        }        
+
+    }
+}
